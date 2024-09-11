85833e01235c7e190ed37439efe072328699c88e
hugopracticas <hugoloyolamaya@gmail.com>
2024-09-11 11:51:58 -0600

    inicio de la aplicacion de gifs

---
+++ src/GiftExpertApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const GiftExpertApp = () => {
+  return (
+    <div>
+      {/**Titulo */}
+      <h1>Gift Expert app</h1>
+
+      {/**Input */}
+
+      {/**Listado gif */}
+    </div>
+  );
+};
+
+export default GiftExpertApp;
