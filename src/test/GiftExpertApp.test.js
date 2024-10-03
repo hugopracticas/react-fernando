c9a931ec40860f3366655eccf38b2695eb3da3f4
hugopracticas <hugoloyolamaya@gmail.com>
2024-10-03 00:05:50 -0600

    Test en GiftExpertApp

--- src/test/GiftExpertApp.test.js
+++ src/test/GiftExpertApp.test.js
@@ -1,9 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { render, renderHook, screen } from "@testing-library/react";
 import GiftExpertApp from "../GiftExpertApp";
 
 describe("Pruebas en GiftExpertApp", () => {
   test("Debe de de renderizar un titulo", () => {
     render(<GiftExpertApp />);
     screen.debug();
+    expect(screen.getByText("Gift Expert app"));
+  });
+
+  test("Debe ", () => {
+    const res = renderHook(() => GiftExpertApp());
   });
 });
