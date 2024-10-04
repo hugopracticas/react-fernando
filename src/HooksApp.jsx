6dd048abeef328b212ab3c6a099b81d54af12e97
hugopracticas <hugoloyolamaya@gmail.com>
2024-10-04 15:56:24 -0600

    hook useState

---
+++ src/HooksApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CounterApp } from "./01-useState/CounterApp";
+import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
+
+export const HooksApp = () => {
+  return (
+    <div>
+      <CounterWithCustomHook />
+    </div>
+  );
+};
+
+//export default HooksApp
